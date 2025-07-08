90ffc296195c336663aa25a1c3e50a433456c1aa
Nadir Kutluozen <nadirkutluozen.nk@gmail.com>
2025-07-08 14:31:50 -0400

    Initial commit

---
+++ online-course-compare/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+      <h1>Online Course Compare</h1>
+      <div className="card">
+        <p>
+           <code>coming soon...</code> 
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
