416a02b76210031a9069d6bb5a64931c14d9a467
Nadir Kutluozen <nadirkutluozen.nk@gmail.com>
2025-07-10 13:58:34 -0400

    changed the style

--- online-course-compare/src/section2/card/card.jsx
+++ online-course-compare/src/section2/card/card.jsx
@@ -3,11 +3,11 @@ import './card.css';
 
 const Card = ({ title, description, image }) => {
     return (
-        <div className="card">
+        <div className="card-custom">
             <img src={image} alt={title} className="card-image" />
             <div className="card-content">
-                <h3 className="card-title">{title}</h3>
-                <p className="card-description">{description}</p>
+                <h3 className="card-tit">{title}</h3>
+                <p className="card-desc">{description}</p>
             </div>
         </div>
     );
