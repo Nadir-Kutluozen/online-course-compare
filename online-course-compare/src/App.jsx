eabd962b9e584d44153e9fb1116f1bd17a2f8f0a
Nadir Kutluozen <nadirkutluozen.nk@gmail.com>
2025-07-23 10:21:23 -0400

    Add vercel.json new stuff

--- online-course-compare/src/App.jsx
+++ online-course-compare/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter  as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
 import Homepage from './landingpage/homepage'
 import Action from './action/action'
