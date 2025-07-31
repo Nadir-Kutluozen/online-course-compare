aff64a308b366d6092d49515e13db9686878c239
Nadir Kutluozen <nadirkutluozen.nk@gmail.com>
2025-07-31 14:05:20 -0400

    added more style and made it smooth

--- online-course-compare/src/header/Header.jsx
+++ online-course-compare/src/header/Header.jsx
@@ -1,18 +1,35 @@
-// src/components/Header.jsx
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import DynamicButton from '../assets/DynamicButton';
-import Squares from '../assets/Squares'; // Assuming you have a Squares component for the logo
-
+import Squares from '../assets/Squares';
 
 const Header = () => {
+  // Collapse navbar on nav-item click
+  useEffect(() => {
+    const navLinks = document.querySelectorAll('.navbar-collapse .nav-link, .navbar-collapse a');
+    const collapseEl = document.getElementById('navbarNav');
+
+    navLinks.forEach((link) => {
+      link.addEventListener('click', () => {
+        const collapseInstance = new window.bootstrap.Collapse(collapseEl, {
+          toggle: false
+        });
+        collapseInstance.hide(); // this will collapse it
+      });
+    });
+
+    return () => {
+      navLinks.forEach((link) => {
+        link.removeEventListener('click', () => {});
+      });
+    };
+  }, []);
+
   return (
-    <nav className="navbar navbar-expand-lg bg-white shadow px-3  " >
-      
+    <nav className="navbar navbar-expand-lg bg-white shadow px-3">
       <div className="container-fluid">
         <Link className="navbar-brand d-flex align-items-center" to="/">
           <img src="/logo.svg" alt="Logo" style={{ height: '40px' }} className="me-2" />
-          
         </Link>
 
         <button
@@ -27,31 +44,31 @@ const Header = () => {
           <span className="navbar-toggler-icon"></span>
         </button>
 
-        <div className="collapse navbar-collapse justify-content-end  " id="navbarNav">
+        <div className="collapse navbar-collapse  justify-content-end" id="navbarNav">
           <ul className="navbar-nav mx-auto text-center gap-3">
             <li className="nav-item">
-              <Link to="/" className=" lead ">
-              <DynamicButton>Home</DynamicButton>
+              <Link to="/" className="nav-link lead">
+                <DynamicButton>Home</DynamicButton>
               </Link>
             </li>
-            <li className="">
-              <a href="/#action" className=" ">
-              <DynamicButton>Compare</DynamicButton>
+            <li className="nav-item">
+              <a href="/#action" className="nav-link">
+                <DynamicButton>Compare</DynamicButton>
               </a>
             </li>
             <li className="nav-item">
-              <Link to="/courses" className="lead">
-              <DynamicButton>Courses</DynamicButton>
+              <Link to="/courses" className="nav-link lead">
+                <DynamicButton>Courses</DynamicButton>
               </Link>
             </li>
             <li className="nav-item">
-              <Link to="/freeCourses" className="lead">
-              <DynamicButton>Free Courses</DynamicButton>
+              <Link to="/freeCourses" className="nav-link lead">
+                <DynamicButton>Free Courses</DynamicButton>
               </Link>
             </li>
             <li className="nav-item">
-              <Link to="/about" className=" lead">
-              <DynamicButton>About</DynamicButton>
+              <Link to="/about" className="nav-link lead">
+                <DynamicButton>About</DynamicButton>
               </Link>
             </li>
           </ul>
